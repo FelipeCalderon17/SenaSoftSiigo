4671ef4f104bc4a9dc2509c79146171d4039339e
FelipeCalderon17 <calderonfelipe017@gmail.com>
2023-09-26 18:57:52 -0500

    fin dia uno

--- front/src/components/Registro.jsx
+++ front/src/components/Registro.jsx
@@ -6,27 +6,28 @@ const Registro = ({ controlador, setControlador }) => {
   console.log(controlador);
   const registrarse = (e) => {
     e.preventDefault();
+    console.log(e.target);
+    let email = e.target.emailRegistro.value;
+    let password = e.target.passRegistro.value;
+    console.log(email);
+    console.log(password);
     fetch(urlApi, {
       method: "POST",
       headers: {
         "content-type": "application/json",
       },
       body: JSON.stringify({
-        email: emailRegistro.value,
-        password: passRegistro.value,
+        email: email,
+        password: password,
       }),
     })
       .then((response) => {
-        return response.text();
+        return response.json();
       })
 
       .then((response) => {
         if (response.affectedRows === 1) {
-          Swal.fire(
-            "Felicitaciones!",
-            "Usuario registrado satisfactoriamente",
-            "success"
-          );
+          Swal.fire("Felicitaciones!", "Usuario registrado satisfactoriamente", "success");
           setTimeout(() => {
             window.location = "index.html";
           }, 1700);
@@ -41,13 +42,7 @@ const Registro = ({ controlador, setControlador }) => {
   };
   return (
     <>
-      <video
-        className="bg-video"
-        playsinline="playsinline"
-        autoplay="autoplay"
-        muted="muted"
-        loop="loop"
-      >
+      <video className="bg-video" playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
         <source src={bg} type="video/mp4" />
       </video>
       <div className="masthead">
@@ -55,7 +50,7 @@ const Registro = ({ controlador, setControlador }) => {
           <div className="container-fluid px-4 px-lg-0">
             <h1 className="fst-italic lh-1 mb-4">Registrar Usuario</h1>
 
-            <form id="contactForm">
+            <form id="contactForm" onSubmit={registrarse}>
               <div className="row input-group-newsletter">
                 <div className="col">
                   <input
@@ -85,26 +80,14 @@ const Registro = ({ controlador, setControlador }) => {
                       Ya tienes cuenta?
                     </a>
                   </div>
-                  <button
-                    className="btn btn-success w-50 mt-2"
-                    type="submit"
-                    id="btnRegistrarse"
-                    onClick={registrarse}
-                  >
-                    Registrarse
-                  </button>
+                  <input className="btn btn-success w-50 mt-2" type="submit" id="btnRegistrarse" />
+                  Registrarse
                 </div>
               </div>
-              <div
-                className="invalid-feedback mt-2"
-                data-sb-feedback="email:required"
-              >
+              <div className="invalid-feedback mt-2" data-sb-feedback="email:required">
                 An email is required.
               </div>
-              <div
-                className="invalid-feedback mt-2"
-                data-sb-feedback="email:email"
-              >
+              <div className="invalid-feedback mt-2" data-sb-feedback="email:email">
                 Email is not valid.
               </div>
 
@@ -113,15 +96,11 @@ const Registro = ({ controlador, setControlador }) => {
                   <div className="fw-bolder">Form submission successful!</div>
                   To activate this form, sign up at
                   <br />
-                  <a href="https://startbootstrap.com/solution/contact-forms">
-                    https://startbootstrap.com/solution/contact-forms
-                  </a>
+                  <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
                 </div>
               </div>
               <div className="d-none" id="submitErrorMessage">
-                <div className="text-center text-danger mb-3 mt-2">
-                  Error sending message!
-                </div>
+                <div className="text-center text-danger mb-3 mt-2">Error sending message!</div>
               </div>
             </form>
           </div>
@@ -129,16 +108,10 @@ const Registro = ({ controlador, setControlador }) => {
       </div>
       <div className="social-icons">
         <div className="d-flex flex-row flex-lg-column justify-content-center align-items-center h-100 mt-3 mt-lg-0">
-          <a
-            className="btn btn-dark m-3"
-            href="https://github.com/StivenHerrera20"
-          >
+          <a className="btn btn-dark m-3" href="https://github.com/StivenHerrera20">
             <i className="bi bi-github"></i>
           </a>
-          <a
-            className="btn btn-dark m-3"
-            href="https://github.com/FelipeCalderon17"
-          >
+          <a className="btn btn-dark m-3" href="https://github.com/FelipeCalderon17">
             <i className="bi bi-github"></i>
           </a>
         </div>
